6641a181135707b85f437c5cf99b689b9834dd72
Oliver <oliverw@rams.colostate.edu>
2021-11-02 16:07:22 -0600

    10.1 11/2 initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("App is running on port 5000");
+});
+
+app.get("/hi", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// app.post
+
+// app.put
---
+++ main.js
@@ -0,0 +1,3 @@
+let weeklyPayFunction = require("./hr.js");
+
+console.log(weeklyPayFunction(1, 4));
